6881eb0271a52715f3986ca51aa93637634b09d0
juhigupta2110 <87846760+juhigupta2110@users.noreply.github.com>
2021-07-30 10:14:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {StyleSheet, Text, View} from 'react-native';
+
+import Navigator from './routes/homeStack';
+
+const App = () => {
+  return <Navigator />;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
